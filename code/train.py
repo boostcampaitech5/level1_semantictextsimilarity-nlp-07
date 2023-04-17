3af2e29782f5020163a530a616fccedef1c626a2
WorldBestProgrammer <dons97@naver.com>
2023-04-17 01:49:13 +0000

    bug fix

--- code/train.py
+++ code/train.py
@@ -303,7 +303,7 @@ if __name__ == '__main__':
                                   verbose=False,            # 중간 출력문을 출력할지 여부. False 시, 없음.
                                   save_last=True,           # last.ckpt 로 저장됨
                                   save_top_k=1,             # k개의 최고 성능 체크 포인트를 저장하겠다.
-                                  save_weights_only=True,   # Weight만 저장할지, 학습 관련 정보도 저장할지 여부.
+                                  save_weights_only=False,   # Weight만 저장할지, 학습 관련 정보도 저장할지 여부.
                                   mode='max',                # 'max' : monitor metric이 증가하면 저장.
                                   dirpath='./checkpoints',
                                   filename=f'{model_name.replace("/","-")}-' + 'sts-{epoch}-{val_pearson:.3f}',
@@ -337,9 +337,9 @@ if __name__ == '__main__':
         if checkpoint_config["checkpoint_name"] != "":
             checkpoint_file = "./checkpoints/" + checkpoint_config['checkpoint_name']
         else:
+            checkpoint_pattern = f"./checkpoints/*.ckpt"
+            checkpoint_files = glob.glob(checkpoint_pattern)
             if checkpoint_files:
-                checkpoint_pattern = f"./checkpoints/*.ckpt"
-                checkpoint_files = glob.glob(checkpoint_pattern)
                 # Sort the list of checkpoint files by val_pearson in descending order
                 if checkpoint_config["checkpoint_new_or_best"].lower() == "best":
                     checkpoint_files = sorted(checkpoint_files, key=extract_val_pearson, reverse=True)
